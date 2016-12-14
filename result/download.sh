dc4f6d8534f9a934114d6e71620fb51db799c54a
morrisyoung <sdmorrisys@gmail.com>
2016-12-13 23:17:31 -0500

    gpu total error cal

--- result/download.sh
+++
@@ -1,3 +0,0 @@
-scp sy2515@login.c2b2.columbia.edu:/ifs/scratch/c2b2/ip_lab/sy2515/GTEx_dnn/workbench2/result/list_error.npy list_error.npy
-scp sy2515@login.c2b2.columbia.edu:/ifs/scratch/c2b2/ip_lab/sy2515/GTEx_dnn/workbench2/result/list_error_test.npy list_error_test.npy
-
